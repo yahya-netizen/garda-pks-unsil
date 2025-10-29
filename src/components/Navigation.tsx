b531a37158084e51b9f9c472aaad86b30af636a0
yahya-netizen <247006111032@student.unsil.ac.id>
2025-10-29 20:55:32 +0700

    Update Logo

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -24,7 +24,7 @@ const Navigation = ({ activeSection, setActiveSection }: NavigationProps) => {
           {/* Logo and Title */}
           <div className="flex items-center space-x-3">
             <div className="flex items-center justify-center w-10 h-10 bg-primary rounded-lg">
-              <img src={"satgas.png"} alt="" />
+              <img src={"satgas.ico"} alt="" />
             </div>
             <div className="hidden lg:block">
               <h1 className="text-lg font-bold text-primary">
